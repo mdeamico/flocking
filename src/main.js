a8a3c7350403dfe371554505c0278de4d447a4e2
mdeamico <33907265+mdeamico@users.noreply.github.com>
2018-12-06 20:15:10 -0600

    Add basic sterring behavior

--- src/main.js
+++ src/main.js
@@ -12,12 +12,44 @@ function main() {
     let canvas = document.getElementById('simulation-canvas');
     let ctx = canvas.getContext('2d');
 
+    let v = new Victor(0,0);
+    console.log(v);
 
+    let target = new Victor(150,150);
+    const DESIRED_SPEED = 6;
+
+    function limitForce(vector) {
+        const maxForce = 0.05;
+        // Limit the vector to a certain length.
+        if (vector.length() > maxForce) {
+            vector.normalize().multiplyScalar(maxForce);
+        }
+        return vector;
+    }
 
     function update() {
+
+
+
         for (let boid of flock) {
-            boid.x += boid.vx;
-            boid.y += boid.vy;
+            // Make decision
+            let desired = target.clone().subtract(boid.pos);
+
+            if (!desired.isZero()) {
+                desired.normalize().multiplyScalar(DESIRED_SPEED);
+            }
+
+            let steeringForce = desired.subtract(boid.v);
+            boid.a = limitForce(steeringForce);
+
+            // Update velocity
+            boid.v.add(boid.a);	
+
+            // Apply velocity to position
+            boid.pos.add(boid.v);
+
+            // Reset acceleration
+            boid.a.zero();
         }
     }
 
@@ -25,7 +57,7 @@ function main() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (let boid of flock) {
             ctx.beginPath();
-            ctx.arc(boid.x, boid.y, 4, 0, Math.PI*2);
+            ctx.arc(boid.pos.x, boid.pos.y, 4, 0, Math.PI*2);
             ctx.fill();
             ctx.closePath();
         }
