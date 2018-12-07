b20b2fc73883bf5672abcec7f5d29005ec41879d
mdeamico <33907265+mdeamico@users.noreply.github.com>
2018-12-06 20:14:36 -0600

    Update Boid to store properties in vectors

--- src/boid.js
+++ src/boid.js
@@ -1,9 +1,15 @@
+function getRand(min, max) {
+    // inclusive
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
 export class Boid {
     constructor() {
         console.log('Boid Constructor');
-        this.x = Math.floor(Math.random() * 300) + 1;
-        this.y = Math.floor(Math.random() * 300) + 1;
-        this.vx = Math.floor(Math.random() * 3) + 1;
-        this.vy = Math.floor(Math.random() * 3) + 1;
+
+        this.pos = new Victor(getRand(0, 300), getRand(0, 300));
+        this.v = new Victor(0,0);
+        this.a = new Victor(0,0);
+
     }
 }
